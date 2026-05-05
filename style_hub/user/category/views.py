d744fdd51c0e242449d48f2a2776a9a230f7eaab
nishad <nishadnishu454@gmail.com>
2026-05-05 10:23:04 +0530

    category_page updated

--- style_hub/user/category/views.py
+++ style_hub/user/category/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from admin_panel.categorymanagement.models import Category
 from admin_panel.productmanagement.models import Product
 
 
-# Create your views here.
-
-
 def category_page(request):
-
-    products=Product.objects.filter(
+    products = Product.objects.filter(
         is_deleted=False,
         is_active=True,
         category__is_deleted=False,
         category__is_active=True
     ).order_by('-id')
-    
-    category=Category.objects.filter(is_active=True,is_deleted=False)
-    
 
-    context={
-        'category':category,
-        'products':products
+    category = Category.objects.filter(
+        is_active=True,
+        is_deleted=False
+    )
+
+    paginator = Paginator(products,8) 
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
+    context = {
+        'category': category,
+        'products': products
     }
-    return render(request,'category_page.html',context)
 
+    return render(request, 'category_page.html', context)
--- style_hub/user/products/views.py
+++ style_hub/user/products/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from admin_panel.productmanagement.models import Product
 from admin_panel.categorymanagement.models import Category
-# Create your views here.
+
 
 def prodcut_page(request):
-
-    products=Product.objects.filter(
+    products = Product.objects.filter(
         is_deleted=False,
         is_active=True,
         category__is_deleted=False,
-        category__is_active=True).order_by('-id')
-    
-    category=Category.objects.filter(
+        category__is_active=True
+    ).order_by('-id')
+
+    categories = Category.objects.filter(
         is_deleted=False,
-        is_active=True)
-    
-    context={
-        'products':products,
-        'category':category
+        is_active=True
+    )
+
+    paginator = Paginator(products, 6)
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
+    context = {
+        'products': products,
+        'categories': categories
     }
 
-    return render(request,'product_page.html',context)
-
-
-
-
-
-
-
-
+    return render(request, 'product_page.html', context)
