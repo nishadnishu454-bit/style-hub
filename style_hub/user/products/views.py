727d15ba1b821f709df60eb7eb331fad7e568097
nishad <nishadnishu454@gmail.com>
2026-05-05 11:02:21 +0530

    product_page updated

--- style_hub/user/products/views.py
+++ style_hub/user/products/views.py
@@ -5,17 +5,37 @@ from admin_panel.categorymanagement.models import Category
 
 
 def prodcut_page(request):
+    sort = request.GET.get('sort', '')
+
     products = Product.objects.filter(
         is_deleted=False,
         is_active=True,
         category__is_deleted=False,
         category__is_active=True
-    ).order_by('-id')
+    )
+
+    if sort == 'price_low':
+        products = products.order_by('price')
+    elif sort == 'price_high':
+        products = products.order_by('-price')
+    else:
+        products = products.order_by('-id')
 
     categories = Category.objects.filter(
         is_deleted=False,
         is_active=True
     )
+
+
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    if max_price:
+        products = products.filter(price__lte=max_price)
+        
 
     paginator = Paginator(products, 6)
     page_number = request.GET.get('page')
@@ -23,7 +43,8 @@ def prodcut_page(request):
 
     context = {
         'products': products,
-        'categories': categories
+        'categories': categories,
+        'sort': sort,
     }
 
     return render(request, 'product_page.html', context)
