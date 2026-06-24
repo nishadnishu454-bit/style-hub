1af4ec620300596d36eb58497a334f1b5bae6395
nishad <nishadnishu454@gmail.com>
2026-06-24 10:50:45 +0530

    home page updated

--- style_hub/user/core/views.py
+++ style_hub/user/core/views.py
@@ -7,27 +7,17 @@ from django.core.exceptions import PermissionDenied
 # Create your views here.
 def home_page(request):
     categories = Category.objects.filter(is_active=True, is_deleted=False)
-    category_map = {}
+    category_map = {}  # ← ith add cheyyu
     for cat in categories:
         name = cat.category_name.lower().strip()
-
-        # T-Shirts
         if 't-shirt' in name or 'tshirt' in name or 't shirt' in name:
             category_map['t_shirts'] = cat.id
-
-        # Shirts
         elif 'shirt' in name:
             category_map['shirts'] = cat.id
-
-        # Pants / Jeans
         elif 'pant' in name or 'jean' in name:
             category_map['pants'] = cat.id
-
-        # Jerseys
         elif 'jersey' in name or 'jersy' in name:
-             category_map['jerseys'] = cat.id
-
-        # Hoodies
+            category_map['jerseys'] = cat.id
         elif 'hoodie' in name or 'hoodies' in name:
             category_map['hoodies'] = cat.id
     return render(request, 'home.html', {'category_map': category_map})
